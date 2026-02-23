25af3793aa5ca980ec951ed2b1bd1d90362e2826
Alberto Miorin <32617+amiorin@users.noreply.github.com>
2026-02-23 08:16:05 +0100

    wip

--- src/walter.clj
+++ src/walter.clj
@@ -4,6 +4,7 @@
    [big-config :as bc]
    [big-config.core :as core]
    [big-config.render :as render]
+   [big-config.run :as run]
    [big-config.step-fns :as step-fns]
    [big-config.utils :refer [debug]]
    [big-config.workflow :as workflow]
@@ -27,7 +28,7 @@
 (defn tofu*
   [args & [opts]]
   (let [opts (merge (workflow/parse-args args)
-{::bc/env :shell}
+                    {::bc/env :shell}
                     opts)]
     (tofu step-fns opts)))
 
@@ -69,8 +70,8 @@
   (-> tap-values))
 
 (defn extract-params
-  [{:keys [::workflow/dirs]}]
-  (let [ip (-> (p/shell {:dir (::tofu dirs)
+  [opts]
+  (let [ip (-> (p/shell {:dir (workflow/path opts ::tofu)
                          :out :string} "tofu show --json")
                :out
                (json/parse-string keyword)
@@ -94,11 +95,10 @@
                                           ::tofu [(partial tofu step-fns) ::ansible]
                                           ::ansible [(partial ansible step-fns) ::end]
                                           ::end [identity]))
-                             :next-fn (fn [step next-step {:keys [::bc/exit ::workflow/dirs] :as opts}]
+                             :next-fn (fn [step next-step {:keys [::bc/exit] :as opts}]
                                         (if (#{::tofu ::ansible} step)
                                           (do
                                             (swap! opts* merge (select-keys opts [::bc/exit ::bc/err]))
-                                            (swap! opts* update ::workflow/dirs merge dirs)
                                             (swap! opts* assoc step opts))
                                           (reset! opts* opts))
                                         (cond
@@ -127,16 +127,33 @@
                      ::workflow/delete-fn resource-delete}
                     opts)
         wf (core/->workflow {:first-step ::start
+                             :last-step ::end-comp
                              :wire-fn (fn [step step-fns]
                                         (case step
                                           ::start [(partial workflow/run-steps step-fns) ::end-comp]
                                           ::end-comp [identity]))})]
     (wf step-fns opts)))
 
+(comment
+  (debug tap-values
+    (let [profile "insta-a"]
+      (resource step-fns
+                {::bc/env :repl
+                 ::workflow/steps [:create]
+                 ::render/profile profile
+                 ::workflow/prefix (format ".dist/%s" profile)
+                 ::workflow/create-opts {::tofu-opts (workflow/parse-args "render tofu:init")
+                                         ::ansible-opts (workflow/parse-args "render")}
+                 ::workflow/delete-opts (workflow/parse-args "render")})))
+  (-> tap-values))
+
 (defn resource*
   [args & [opts]]
-  (let [opts (merge (workflow/parse-args args)
-                    {::bc/env :shell}
+  (let [profile "insta-a"
+        opts (merge (workflow/parse-args args)
+                    {::bc/env :shell
+                     ::render/profile profile
+                     ::workflow/prefix (format ".dist/%s" profile)}
                     opts)]
     (resource step-fns opts)))
 
